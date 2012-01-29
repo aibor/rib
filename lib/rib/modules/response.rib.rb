8c0892d45c877a651e42426092618c3fed548107
Tobias Böhm <aibo@aibor.de>
2012-01-29 17:44:26 +0100

    restructured code, modularized

--- lib/rib/modules/response.rib.rb
+++ lib/rib/modules/response.rib.rb
@@ -4,13 +4,13 @@ module RIB
     class Response
       RESP = { "hi"   => ["hi", "Moin!", "Tag"],
                "bye"  => ["nein?", "orrr, nö!"],
-               "rage" => ["\u0028\u256f\u00b0\u25a1\u00b0\u0029\u256f\ufe35\u0020\u253b\u2501\u253b"]}
+               "rage" => ['\u0028\u256f\u00b0\u25a1\u00b0\u0029\u256f\ufe35\u0020\u253b\u2501\u253b']}
       TRIGGER = /\A#{RIB::TC}(hi|bye|rage)\Z/
       
       def output( s, m )
         key = RESP[m[1]]
         out = key[rand(key.length)]
-        out.gsub(/\\/, '\\\\') if RUBY_VERSION < '1.9'
+        #out.gsub(/\\/, '\\\\') if RUBY_VERSION < '1.9'
         return nil, out
       end
     end
