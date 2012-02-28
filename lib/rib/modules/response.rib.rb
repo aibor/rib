9b1af0d45a8682c2a8e38c061d49edddabf590e4
Tobias Böhm <aibo@aibor.de>
2012-02-28 22:25:30 +0100

    link nonsense

--- lib/rib/modules/response.rib.rb
+++ lib/rib/modules/response.rib.rb
@@ -2,11 +2,13 @@
 module RIB
   module MyModules
     class Response
-      RESP = { "hi"   => ["hi", "Moin!", "Tag"],
-               "bye"  => ["nein?", "orrr, nö!"],
+      RESP = { "hi"   => ["hi", "Moin!", "Tag", "Ahoj!", "Servus!"],
+               "bye"  => ["nein?", "orrr, nö!", "selber!", "°_°"],
       # ruby > 1.9  "rage" => ["\\u0028\\u256f\\u00b0\\u25a1\\u00b0\\u0029\\u256f\\ufe35\\u0020\\u253b\\u2501\\u253b"]}
-               "rage" => ["(╯°□°)╯︵ ┻━┻"]}
-      TRIGGER = /\A#{RIB::TC}(hi|bye|rage)\Z/
+               "rage" => ["(╯°□°)╯︵ ┻━┻"],
+               "panic" => ["https://dl.dropbox.com/u/6670723/images/panic.gif"],
+               "alone" => ["https://dl.dropbox.com/u/6670723/images/forever_alone.png"]}
+      TRIGGER = /\A#{RIB::TC}(hi|bye|rage|panic|alone)\Z/
       
       def output( s, m )
         key = RESP[m[1]]
