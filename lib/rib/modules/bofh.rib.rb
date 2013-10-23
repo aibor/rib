867e42942846744158fcde5eadbc21d60ba27119
Tobias Böhm <aibo@aibor.de>
2013-10-23 22:14:14 +0200

    added quote num selection to bofh

--- lib/rib/modules/bofh.rib.rb
+++ lib/rib/modules/bofh.rib.rb
@@ -3,13 +3,15 @@ module RIB
   module MyModules
     class Bofhquote < RIB::MyModulesBase
       def trigger
-        /\A#{@bot.config.tc}bofh/
+        /\A#{@bot.config.tc}bofh(?:\s(\d+))?/
       end
       
       def output( s, m, c )
         quotefile = File.expand_path("../bofhquotes", __FILE__)
         quotes = File.readlines(quotefile).each {|l| l.strip!}
-        quote = quotes[rand(quotes.size)].split(/ \| /)
+        num = m[1].to_i.pred
+        index = ((0..quotes.size.pred).include? num) ? num : rand(quotes.size)
+        quote = quotes[index].split(/ \| /)
         out = "#{quote[0]}: " + quote[1]
         return nil, out
       end
