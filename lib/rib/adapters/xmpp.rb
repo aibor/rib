a4e153279058c6f9901cd3277c44c0591a8120cc
Tobias Böhm <aibo@aibor.de>
2014-12-30 14:58:34 +0100

    fixed XMPP adapter

--- lib/rib/adapters/xmpp.rb
+++ lib/rib/adapters/xmpp.rb
@@ -1,23 +1,31 @@
 # coding: utf-8
 
 require 'rib'
+require 'xmpp4r'
+require 'xmpp4r/roster'
+require 'xmpp4r/client'
+require 'xmpp4r/muc'
 
 
 class RIB::Adapters::XMPP
 
   include RIB::Adaptable
+  include RIB::Connection::Logable
+
 
   def initialize(config, log_path)
-    Jabber::debug = true if config.debug
+    ::Jabber::debug = true if config.debug
 
-    @connection = Connection.new(
-      log_path, config.server, config.nick, config.jid
-    )
-    @connection.login
-    @connection.auth_nick(config.auth)
-    @connection.togglelogging
+    @hostname = config.server
+    @resource = config.nick
+    @log_path = log_path
+    jid = ::Jabber::JID.new(config.jid + "/" + @resource)
+    @client = ::Jabber::Client.new(jid)
+    @muc = {}
+
+    login config.auth
     config.channel.split(/\s+|\s*,\s*/).each do |chan|
-      @connection.join_channel(chan)
+      join_channel(chan)
     end
   end
 
@@ -26,9 +34,9 @@ class RIB::Adapters::XMPP
   # @todo what a mess
 
   def run_loop
-    @connection.muc.each do |room,muc|
+    @muc.each do |room,muc|
       muc.on_message do |time, nick, text|
-        next if nick == @connection.resource
+        next if nick == @resource
 
         begin
           rib_msg = RIB::Message.new(text, nick, room)
@@ -47,7 +55,46 @@ class RIB::Adapters::XMPP
 
   def say(line, target)
     line.encode("utf-8")
-    target.say(line) if target.is_a? Jabber::MUC::SimpleMUCClient
+    target.say(line) if target.is_a? ::Jabber::MUC::SimpleMUCClient
+  end
+
+
+  def quit(msg = 'Bye!')
+    @muc.each_value {|m| m.say msg} if msg
+    @client.close
+  end
+
+
+  private
+
+  def join_channel(channel)
+    mucjid = ::Jabber::JID.new "#{channel}@#{@hostname}/#{@resource}"
+    @muc[channel.to_sym] = ::Jabber::MUC::SimpleMUCClient.new @client
+    @muc[channel.to_sym].join mucjid
+    add_ping_cb
+  end
+
+
+  def login(password)
+    @client.connect
+    @client.auth password
+    @client.send ::Jabber::Presence.new.set_type(':available')
+    togglelogging
+  end
+
+
+  def add_ping_cb
+    @client.add_iq_callback do |iq_received|
+      if iq_received.type == :get
+        if iq_received.queryns.to_s != 'http://jabber.org/protocol/disco#info'
+          iq = ::Jabber::Iq.new :result, @client.jid.node
+          iq.id = iq_received.id
+          iq.from = iq_received.to
+          iq.to = iq_received.from
+          @client.send iq
+        end
+      end
+    end
   end
 
 end
