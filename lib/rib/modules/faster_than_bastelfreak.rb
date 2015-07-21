e44fd38bfef5fa8945c30f2c190300b2632a4215
Tobias Böhm <code@aibor.de>
2015-07-21 21:53:58 +0200

    fixed the module

--- lib/rib/modules/faster_than_bastelfreak.rb
+++ lib/rib/modules/faster_than_bastelfreak.rb
@@ -3,28 +3,22 @@ require'net/http'
 
 class RIB::Module::FasterThanBastelfreak < RIB::Module
 
-  describe ftb: 'Benchmark the given URL against bastelfreaks blog'
-  
   BASE_URL = 'https://flipez.de/ftb/api?q='
 
   timeout ftb: 10
 
-  def ftb url
-    response = req URI(url)
-    score = parse_score response.body
-
-    "#{uri.hostname} reached a FTB™ Score of #{score}"
+  describe 'Benchmark the given URL against bastelfreaks blog'
+  def ftb(url)
+    uri = URI("#{BASE_URL}#{url}")
+    "%s reached a FTB™ Score of %s" % [uri.hostname, get_score uri]
   end
 
   private
 
-  def req url
-    uri = URI("#{BASE_URL}#{url}")
-    Net::HTTP.get_response uri
-  end
-
-  def parse_score json
-    result = JSON.parse(json)
+  def get_score(uri)
+    response  = Net::HTTP.get_response uri
+    result    = JSON.parse(response.body)
     result['test']['result']
   end
 end
+
